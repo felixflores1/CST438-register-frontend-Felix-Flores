3d1cbc11266df905225f0dad6fc9cbaa0ee4d1eb
felixflores1 <123616125+felixflores1@users.noreply.github.com>
2023-11-02 11:15:57 -0700

    Update EditStudent.js

--- src/components/EditStudent.js
+++ src/components/EditStudent.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const EditStudent = (props)  => {
+const EditStudent = (props) => {
+    const [studentName, setStudentName] = useState(props.student.name);
 
+    const handleNameChange = (event) => {
+        setStudentName(event.target.value);
+    };
+
+    const handleSave = () => {
+  
+        console.log('Updated student name:', studentName);
+
+    };
 
     return (
         <div>
-        </div>                       
-    )
+            <h2>Edit Student</h2>
+            <label htmlFor="studentName">Student Name:</label>
+            <input
+                type="text"
+                id="studentName"
+                value={studentName}
+                onChange={handleNameChange}
+            />
+            <button onClick={handleSave}>Save</button>
+        </div>
+    );
 }
 
 export default EditStudent;
